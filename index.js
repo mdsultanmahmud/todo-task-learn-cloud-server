5543f186f1c5ed8c534ef84258c51cdb4dd203e1
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-02-27 18:05:38 +0600

    add task done

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
 const port = 5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 // password: 3PSQHEdeb_s$#SB
 // username: TodoApp
 
+// middleware 
+app.use(cors())
+app.use(express.json())
 
 
-
-const uri = "mongodb+srv://TodoApp:3PSQHEdeb_s$#SB@cluster0.p11nzlu.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://Task_Manager:l22nvLd0mbIff8Ij@cluster0.p11nzlu.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
-
+        const TodoApp  = client.db('TodoApp').collection('todo_tasks')
+        app.post('/addtask', async (req, res) =>{
+            const task = req.body
+            const result = await TodoApp.insertOne(task)
+            res.send(result)
+        })
     }
     catch {
 
     }
 }
 
-run.catch(err => console.log(err))
+run().catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('Hello, task server is running from')
